799f6281dd24efd50779d47ae5f885e1f36b154a
thelastmanchan <a01044100069@gmail.com>
2024-10-26 14:50:13 +0900

    HTML 태그 배우기

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def Faker():
-    return 'Faker'
+def hello_world():
+    return render_template("index.html", title="민찬의 홈페이지", message="환영합니다")
 
 if __name__ == '__main__':
     app.run(debug=True)
